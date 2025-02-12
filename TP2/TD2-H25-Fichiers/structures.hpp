44159c76acc2fa67c28b686e350259719c92ea94
Antoine925637 <antoine.grondin@polymtl.ca>
2025-02-12 17:47:13 -0500

    partie 2 pas bonne structures.hpp

--- TP2/TD2-H25-Fichiers/structures.hpp
+++ TP2/TD2-H25-Fichiers/structures.hpp
@@ -1,29 +1,52 @@
 #pragma once
-// Structures mémoires pour une collection de films.
 
 #include <string>
 
-struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
+struct Film; struct Acteur; 
 
-struct ListeFilms {
-	int capacite, nElements;
-	Film** elements; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
+class ListeFilms {
+public:
+    ListeFilms() = default;
+
+    ~ListeFilms() {
+        detruireListeFilms();
+    }
+
+    int getNElements() const {
+        return nElements;
+    }
+
+    Film** getElements() const {
+        return elements;
+    }
+
+    void ajouterFilm(Film* film);
+    void supprimerFilm(Film* film);
+    void detruireListeFilms();
+    void afficherListeFilms() const;
+
+private:
+    int capacite = 0, nElements = 0;
+    Film** elements = nullptr;
 };
 
 struct ListeActeurs {
-	int capacite, nElements;
-	Acteur** elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
+    int capacite, nElements;
+    Acteur** elements;
 };
 
-struct Film
-{
-	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
-	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
-	ListeActeurs acteurs;
+struct Film {
+    std::string titre, realisateur;
+    int anneeSortie, recette;
+    ListeActeurs acteurs;
 };
 
-struct Acteur
-{
-	std::string nom; int anneeNaissance; char sexe;
-	ListeFilms joueDans;
+struct Acteur {
+    std::string nom;
+    int anneeNaissance;
+    char sexe;
+    ListeFilms joueDans;
 };
+
+void detruireFilm(Film* film); 
+void afficherFilm(const Film* film);
