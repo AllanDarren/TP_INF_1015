b298019aecfd46e92572e8af2f8d7afda790fac0
Antoine925637 <antoine.grondin@polymtl.ca>
2025-02-18 14:35:26 -0500

    Update structures.hpp

--- TP2/TD2-H25-Fichiers/structures.hpp
+++ TP2/TD2-H25-Fichiers/structures.hpp
@@ -1,52 +1,50 @@
 #pragma once
+// Structures mémoires pour une collection de films.
 
 #include <string>
+#include <memory>
+using namespace std;
 
-struct Film; struct Acteur; 
-
+struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
 class ListeFilms {
-public:
-    ListeFilms() = default;
-
-    ~ListeFilms() {
-        detruireListeFilms();
-    }
-
-    int getNElements() const {
-        return nElements;
-    }
-
-    Film** getElements() const {
-        return elements;
-    }
-
-    void ajouterFilm(Film* film);
-    void supprimerFilm(Film* film);
-    void detruireListeFilms();
-    void afficherListeFilms() const;
-
 private:
-    int capacite = 0, nElements = 0;
-    Film** elements = nullptr;
+	int capacite;
+	int nElements;
+	Film** elements; // Pointeur vers un tableau de Film*
+public:
+	ListeFilms();
+
+	void ajouterFilm(Film* film);
+	void supprimerFilm(Film* film);
+	int size() const;
+	Film* operator[](int index) const;
+	void afficher() const;
+	void detruireListeFilms();
+	Acteur* trouverActeur(const std::string& nom) const;
+	Film** data();
 };
 
 struct ListeActeurs {
-    int capacite, nElements;
-    Acteur** elements;
+	int capacite, nElements;
+	unique_ptr<Acteur*[]> elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
+
+	ListeActeurs(int nElements) :
+		elements(make_unique<Acteur* []>(nElements)) {}
 };
 
-struct Film {
-    std::string titre, realisateur;
-    int anneeSortie, recette;
-    ListeActeurs acteurs;
+struct Film
+{
+	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
+	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
+	ListeActeurs acteurs;
+
+	Film(string titre, string realisateur, int anneeSortie, int recette, int nActeurs)
+		: titre(titre), realisateur(realisateur), anneeSortie(anneeSortie), recette(recette), acteurs(nActeurs) {
+	}
 };
 
-struct Acteur {
-    std::string nom;
-    int anneeNaissance;
-    char sexe;
-    ListeFilms joueDans;
+struct Acteur
+{
+	std::string nom; int anneeNaissance; char sexe;
+	ListeFilms joueDans;
 };
-
-void detruireFilm(Film* film); 
-void afficherFilm(const Film* film);
